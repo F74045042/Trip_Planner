c0cd07e9431b63c566facdb8ab43b2dc6f927b39
F74045042 <sunshangjih0819@gmail.com>
2018-07-15 02:34:51 +0800

    nothing

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().distance(function(d) {
-        return 5*d.cost;
+        return 5 * d.cost;
     }).strength(1).id(function(d) {
         return d.id;
     }))
