13939832b9e959239e4e6665455015190061efb2
F74045042 <sunshangjih0819@gmail.com>
2018-07-19 20:14:34 +0800

    spining loader added

--- test.js
+++ test.js
@@ -159,9 +159,37 @@ var tooltip = d3.select("body")
     .style("visibility", "hidden")
     .text("Text");
 
+// Loader
+var target = document.getElementById('loader');
+
+var opts = {
+    lines: 11, // The number of lines to draw
+    length: 6, // The length of each line
+    width: 15, // The line thickness
+    radius: 47, // The radius of the inner circle
+    scale: 1.3, // Scales overall size of the spinner
+    corners: 1, // Corner roundness (0..1)
+    color: '#000000', // CSS color or array of colors
+    fadeColor: 'transparent', // CSS color or array of colors
+    speed: 1, // Rounds per second
+    rotate: 0, // The rotation offset
+    animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    className: 'spinner', // The CSS class to assign to the spinner
+    top: '50%', // Top position relative to parent
+    left: '50%', // Left position relative to parent
+    shadow: '0 0 1px transparent', // Box-shadow for the lines
+    position: 'absolute' // Element positioning
+};
+
+var spinner = new Spinner(opts).spin(target);
 
 d3.json("http://localhost:8000/test.json", function(error, graph) {
     if (error) throw error;
+
+    // stop the loader
+    spinner.stop();
 
     var link = svg.append("g")
         .attr("class", "links")
@@ -275,7 +303,7 @@ d3.json("http://localhost:8000/test.json", function(error, graph) {
     genPath(graph.nodes[1], 500, newPath, 1);
 
     // console.log(poiIDX.nodeIDX(0));
-    // showPath(poiIDX.head.next.down.path);
+    showPath(poiIDX.head.next.down.path);
 
 
 
