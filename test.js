758a5812f56dbf06e2e8b4ff399438575b67b84a
RandyChang <randychang.10@gmail.com>
2018-07-25 18:40:33 +0800

    fix getCost NaN bug & added restaurant/hotel nodes

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 function POIList() {
     this.head = null;
-
+    this.length = 0;
 
     // --------------- member methods --------------- //
     // append poi node
@@ -18,6 +17,7 @@ function POIList() {
             }
             curr.next = newNode;
         }
+        this.length++;
 
     }
 
@@ -53,6 +53,7 @@ function POIList() {
                 prev.next = curr.next; //delete curr (last node)
             }
         }
+        this.length--;
     }
 
     // check if path contains node
@@ -83,19 +84,32 @@ function POIList() {
             for (var i = 0; i < graph.nodes.length; i++) {
                 if ((graph.links[i].source.id == curr.id && graph.links[i].target.id == curr.next.id) ||
                     (graph.links[i].target.id == curr.id && graph.links[i].source.id == curr.next.id)) {
-                    link_cost = graph.links[i].cost;
+                    cost += graph.links[i].cost;
                     // console.log( "link between " + graph.links[i].source.id 
                     //     + " & " + graph.links[i].target.id 
                     //     + " link_cost = " + link_cost);
                     break;
                 }
             }
-            cost += link_cost;
             curr = curr.next;
         }
         // last node: simply add up its avg. stay time
         cost += curr.time; // console.log("last node -> " + curr.id + ":" + curr.time);
         return cost;
+    }
+
+    // get total weight (for sorting by path importance)
+    this.getTotalWeight = function(graph) {
+        let curr = this.head;
+        var weight = 0;
+
+        while (curr.next) {
+            // add up weight at a poi node
+            weight += curr.weight;
+            curr = curr.next;
+        }
+
+        return weight;
     }
 
     // check empty
@@ -114,7 +128,7 @@ function POIList() {
 
 }
 
-// new node function
+// new node constructor
 function node(id, weight, time, next, down) {
     this.id = id;
     this.weight = weight;
@@ -123,7 +137,7 @@ function node(id, weight, time, next, down) {
     this.down = down;
 }
 
-// new path function
+// new path constructor
 function path(new_path, total_cost, total_weight, down) {
     this.path = _.cloneDeep(new_path);
     this.total_cost = total_cost;
@@ -160,6 +174,7 @@ var tooltip = d3.select("body")
     .style("visibility", "hidden")
     .text("Text");
 
+
 // Loader
 var target = document.getElementById('loader');
 
@@ -189,7 +204,7 @@ var spinner = new Spinner(opts).spin(target);
 d3.json("http://localhost:8000/test.json", function(error, graph) {
     if (error) throw error;
 
-    // stop the loader
+    // stop loader
     spinner.stop();
 
     var link = svg.append("g")
@@ -297,451 +312,17 @@ d3.json("http://localhost:8000/test.json", function(error, graph) {
 
     // poi index
     for (var i = 0; i < graph.nodes.length; i++) {
-        poiIDX.addNode(graph.nodes[i].id, graph.nodes[i].weight, graph.nodes[i].time);
-    }
-
-    var newPath = new POIList();
-    genPath(graph.nodes[1], 500, newPath, 1);
-
-
-
-
-
-
-
-
-
-
-
-
-    // ----------------------------------------------------------------- //
-
-
-
-
-
-    // ----------------------- self-defined functions -------------------- //
-
-    // check if a node is connected to the last poi in a path
-    function isConnected(path, node) {
-        // iterate to last node in path
-        let curr = path.head;
-        while (curr.next) {
-            curr = curr.next;
-        }
-
-        // check connectivity 
-        for (var i = 0; i < graph.links.length; i++) {
-
-            // source = path.lastNode && target = nodeToCheck OR vice versa
-            if ((graph.links[i].source.id == curr.id && graph.links[i].target.id == node.id) ||
-                (graph.links[i].target.id == curr.id && graph.links[i].source.id == node.id))
-                return true;
-        }
-        return false;
-    }
-
-    // Return the line between node1 and node2
-    function getLine(id1, id2) {
-        for (var i = 0; i < graph.links.length; i++) {
-            if ((graph.links[i].source.id == id1 && graph.links[i].target.id == id2) ||
-                (graph.links[i].target.id == id1 && graph.links[i].source.id == id2)) {
-                // console.log(d3.selectAll("line")._groups[0][i]);
-                return d3.selectAll("line")._groups[0][i];
-            }
-        }
-        return null;
-    }
-
-    // generate all path with total_cost(link.cost + node.time) under a given threshold from a certain start point  
-    function genPath(node, threshold, newPath, idx) {
-
-        // append to path
-        newPath.addNode(node.id, node.weight, node.time);
-        var tot_cost = newPath.getTotalCost(graph);
-
-        if (threshold > tot_cost) {
-            for (let i = 0; i < graph.nodes.length; i++) {
-                if (isConnected(newPath, graph.nodes[i]) &&
-                    !newPath.contains(graph.nodes[i])) {
-                    // able to append node to path
-                    genPath(graph.nodes[i], threshold, newPath, idx);
-                }
-            }
-
-        }
-
-        // store path
-        poiIDX.addPath(newPath, newPath.getTotalCost(graph), 200, idx);
-        newPath.popNode();
-        return;
-
-    }
-
-    // Reduce the opacity of all node but the best path
-    // can be optimize: each node save its own index, don't have to search all node over and over again.
-    function showPath(path) {
-        let curr = path.head;
-        let d;
-        circle.style("opacity", 0.1);
-        link.style("opacity", 0.1);
-        while (curr) {
-            //change node opacity
-            for (var i = 0; i < graph.nodes.length; i++) {
-                d = d3.selectAll("circle")._groups[0][i];
-                if (d.__data__.id == curr.id) {
-                    console.log(curr.id);
-                    d.style.opacity = 1;
-                    break;
-                }
-            }
-            // console.log(curr);
-            // console.log(d);
-            curr = curr.next;
-            //change line opacity
-            if (curr) {
-                getLine(d.__data__.id, curr.id).style.opacity = 1;
-            }
-        }
-    }
-
-
-});
-
-// zoomable
-var zoom = d3.zoom()
-    .translateExtent([
-        [0, 0],
-        [width, height]
-    ])
-    .scaleExtent([1, 10])
-    .extent([
-        [0, 0],
-        [width, height]
-    ])
-    .on("zoom", function() {
-        svg.selectAll("g").attr("transform", d3.event.transform);
-        svg.selectAll("line").attr("transform", d3.event.transform);
-    });
-
-svg.call(zoom);
-
-function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-}
-
-function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-}
-
-function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-}
-=======
-function POIList() {
-    this.head = null;
-
-    // --------------- member methods --------------- //
-    // append poi node
-    this.addNode = function(id, weight, time) {
-        const newNode = new node(id, weight, time, null, null);
-        let curr;
-
-        if (this.head == null) {
-            this.head = newNode;
-        } else {
-            curr = this.head;
-            while (curr.next) {
-                curr = curr.next;
-            }
-            curr.next = newNode;
-        }
-
-    }
-
-    // add poi path
-    this.addPath = function(new_path, tot_cost, tot_weight, idx) {
-        const newPath = new path(new_path, tot_cost, tot_weight, null);
-        let curr;
-
-        curr = this.nodeIDX(idx);
-        while (curr.down) {
-            curr = curr.down;
-        }
-        curr.down = newPath;
-
-    }
-
-    // pop last poi node
-    this.popNode = function() {
-        let curr;
-        let prev;
-        if (this.head == null) {
-            console.log("list already empty!!");
-        } else {
-            curr = this.head;
-            while (curr.next) {
-                prev = curr;
-                curr = curr.next;
-            }
-
-            if (curr == this.head) { //if we're deleting the first node
-                head = curr.next;
-            } else {
-                prev.next = curr.next; //delete curr (last node)
-            }
-        }
-    }
-
-    // check if path contains node
-    this.contains = function(node) {
-        let curr = this.head;
-
-        while (curr.next) {
-            if (node.id == curr.id) {
-                return true;
-            }
-            curr = curr.next;
-        }
-
-        return false;
-    }
-
-    // get total cost of path
-    this.getTotalCost = function(graph) {
-        let curr = this.head;
-        var cost = 0;
-
-        while (curr.next) {
-            // add up average stay time at a poi node
-            cost += curr.time; // console.log(curr.id + ":" + curr.time);
-
-            // add up cost between this & next poi node
-            var link_cost;
-            for (var i = 0; i < graph.nodes.length; i++) {
-                if ((graph.links[i].source.id == curr.id && graph.links[i].target.id == curr.next.id) ||
-                    (graph.links[i].target.id == curr.id && graph.links[i].source.id == curr.next.id)) {
-                    link_cost = graph.links[i].cost;
-                    // console.log( "link between " + graph.links[i].source.id 
-                    //     + " & " + graph.links[i].target.id 
-                    //     + " link_cost = " + link_cost);
-                    break;
-                }
-            }
-            cost += link_cost;
-            curr = curr.next;
-        }
-        // last node: simply add up its avg. stay time
-        cost += curr.time; // console.log("last node -> " + curr.id + ":" + curr.time);
-        return cost;
-    }
-
-    // get total weight (for sorting by path importance)
-    this.getTotalWeight = function(graph) {
-        let curr = this.head;
-        var weight = 0;
-
-        while (curr.next) {
-            // add up weight at a poi node
-            weight += curr.weight;
-            curr = curr.next;
-        }
-
-        return weight;
-    }
-
-    // check empty
-    this.isEmpty = function() {
-        return (this.head == null);
-    }
-
-    // input index, return node
-    this.nodeIDX = function(index) {
-        let curr = this.head;
-        while (index--) {
-            curr = curr.next;
-        }
-        return curr;
-    }
-
-}
-
-// new node constructor
-function node(id, weight, time, next, down) {
-    this.id = id;
-    this.weight = weight;
-    this.time = time;
-    this.next = next;
-    this.down = down;
-}
-
-// new path constructor
-function path(new_path, total_cost, total_weight, down) {
-    this.path = _.cloneDeep(new_path);
-    this.total_cost = total_cost;
-    this.total_weight = total_weight;
-    this.down = down;
-}
-
-
-
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
-
-var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().distance(function(d) {
-        return 5 * d.cost;
-    }).strength(1).id(function(d) {
-        return d.id;
-    }))
-    .force("charge", d3.forceManyBody().strength(-20))
-    .force("center", d3.forceCenter(width / 2, height / 2))
-    .force('collision', d3.forceCollide().radius(function(d) {
-        return d.weight
-    }));
-
-var tooltip = d3.select("body")
-    .append("div")
-    .style("position", "absolute")
-    .style("color", "blue")
-    .style("font-size", "13px")
-    .style("z-index", "10")
-    .style("visibility", "hidden")
-    .text("Text");
-
-
-d3.json("http://127.0.0.1:8000/test.json", function(error, graph) {
-    if (error) throw error;
-
-    var link = svg.append("g")
-        .attr("class", "links")
-        .selectAll("line")
-        .data(graph.links)
-        .enter().append("line");
-
-    // mouseover event
-    link.on("mouseover", function(d) {
-            tooltip.style("color", "#33CC33FF");
-            tooltip.text("Cost=" + d.cost);
-            return tooltip.style("visibility", "visible");
-        })
-        .on("mousemove", function() {
-            return tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px");
-        })
-        .on("mouseout", function() {
-            return tooltip.style("visibility", "hidden");
-        });
-
-    var node = svg.append("g")
-        .attr("class", "nodes")
-        .selectAll("g")
-        .data(graph.nodes)
-        .enter().append("g");
-
-    var circle = node.append("circle")
-        .attr("r", function(d) {
-            return d.weight;
-        })
-        .attr("fill", function(d) {
-            return color(d.time);
-        })
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
-
-    // mouseover event
-    circle.on("mouseover", function(d) {
-            tooltip.style("color", "#FF6666FF")
-            tooltip.text("Time=" + d.time);
-            return tooltip.style("visibility", "visible");
-        })
-        .on("mousemove", function() {
-            return tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px");
-        })
-        .on("mouseout", function() {
-            return tooltip.style("visibility", "hidden");
-        });
-
-    // show ID beside node
-    var label = node.append("text")
-        .text(function(d) {
-            return d.id;
-        })
-        .attr("dx", 10)
-        .attr("dy", 0);
-
-    simulation
-        .nodes(graph.nodes)
-        .on("tick", ticked);
-
-    simulation.force("link")
-        .links(graph.links);
-
-    function ticked() {
-        link
-            .attr("x1", function(d) {
-                return d.source.x;
-            })
-            .attr("y1", function(d) {
-                return d.source.y;
-            })
-            .attr("x2", function(d) {
-                return d.target.x;
-            })
-            .attr("y2", function(d) {
-                return d.target.y;
-            });
-
-        circle
-            .attr("cx", function(d) {
-                return d.x;
-            })
-            .attr("cy", function(d) {
-                return d.y;
-            });
-
-        label
-            .attr("x", function(d) {
-                return d.x;
-            })
-            .attr("y", function(d) {
-                return d.y;
-            });
-    }
-
-
-
-    // ------------------------- start planning ------------------------ //
-
-    const poiIDX = new POIList();
-
-    // poi index
-    for (var i = 0; i < graph.nodes.length; i++) {
-        poiIDX.addNode(graph.nodes[i].id, graph.nodes[i].weight, graph.nodes[i].time);
+        if (graph.nodes[i].type === "tourist_attraction")
+            poiIDX.addNode(graph.nodes[i].id, graph.nodes[i].weight, graph.nodes[i].time);
     }
 
     // generate path starting from each node and store under poiIDX
-    for (var idx = 0; idx < graph.nodes.length; idx++) {
+    for (var idx = 0; idx < poiIDX.length; idx++) {
         var newPath = new POIList();
-        genPath(graph.nodes[idx], 250, newPath, idx);
+        genPath(poiIDX.nodeIDX(idx), 200, newPath, idx);
     }
 
 
-    // test: returns sorted path list of poiIDX[0] as array
-
-    //console.log(sortByWeight(1));
-
-    // test: poiIDXs
-    console.log(poiIDX.nodeIDX(0));
 
     // generating all path array
     //console.log(genAllPathArr()); 
@@ -784,7 +365,20 @@ d3.json("http://127.0.0.1:8000/test.json", function(error, graph) {
         return false;
     }
 
+    // Return the line between node1 and node2
+    function getLine(id1, id2) {
+        for (var i = 0; i < graph.links.length; i++) {
+            if ((graph.links[i].source.id == id1 && graph.links[i].target.id == id2) ||
+                (graph.links[i].target.id == id1 && graph.links[i].source.id == id2)) {
+                // console.log(d3.selectAll("line")._groups[0][i]);
+                return d3.selectAll("line")._groups[0][i];
+            }
+        }
+        return null;
+    }
+
     // generate all path with total_cost(link.cost + node.time) under a given threshold from a certain start point  
+    // only consider nodes that are of type "tourist_attraction"
     function genPath(node, threshold, newPath, idx) {
 
         // append to path
@@ -794,7 +388,8 @@ d3.json("http://127.0.0.1:8000/test.json", function(error, graph) {
         if (threshold > tot_cost) {
             for (var i = 0; i < graph.nodes.length; i++) {
                 if (isConnected(newPath, graph.nodes[i]) &&
-                    !newPath.contains(graph.nodes[i])) {
+                    !newPath.contains(graph.nodes[i]) &&
+                    graph.nodes[i].type === "tourist_attraction") {
                     // able to append node to path
                     genPath(graph.nodes[i], threshold, newPath, idx);
                 }
@@ -807,6 +402,33 @@ d3.json("http://127.0.0.1:8000/test.json", function(error, graph) {
         newPath.popNode();
         return;
 
+    }
+
+    // Reduce the opacity of all node but the best path
+    // can be optimize: each node save its own index, don't have to search all node over and over again.
+    function showPath(path) {
+        let curr = path.head;
+        let d;
+        circle.style("opacity", 0.1);
+        link.style("opacity", 0.1);
+        while (curr) {
+            //change node opacity
+            for (var i = 0; i < graph.nodes.length; i++) {
+                d = d3.selectAll("circle")._groups[0][i];
+                if (d.__data__.id == curr.id) {
+                    console.log(curr.id);
+                    d.style.opacity = 1;
+                    break;
+                }
+            }
+            // console.log(curr);
+            // console.log(d);
+            curr = curr.next;
+            //change line opacity
+            if (curr) {
+                getLine(d.__data__.id, curr.id).style.opacity = 1;
+            }
+        }
     }
 
     // convert path objects list to array and sort by weight, then return the sorted array
@@ -833,13 +455,13 @@ d3.json("http://127.0.0.1:8000/test.json", function(error, graph) {
         var pathArr = [];
 
         //2-d array generating
-        for (var idx = 0; idx < graph.nodes.length; idx++) {
+        for (var idx = 0; idx < poiIDX.length; idx++) {
             pathArr[idx] = sortByWeight(idx);
         }
         return pathArr;
     }
 
-    
+
     function genMultiItinerary(Arr) {
         ReArr = remain(Arr);
         ReMax = getMaxWeight2(ReArr);
@@ -967,7 +589,7 @@ d3.json("http://127.0.0.1:8000/test.json", function(error, graph) {
 
 });
 
-//zoomable
+// zoomable
 var zoom = d3.zoom()
     .translateExtent([
         [0, 0],
@@ -1003,4 +625,3 @@ function dragended(d) {
     d.fx = null;
     d.fy = null;
 }
->>>>>>> 6cd3173edb515301097749a15470300658ef93b8
