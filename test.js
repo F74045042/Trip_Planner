55c435bab323b1048ec80f0e650925eeb614f342
RandyChang <randychang.10@gmail.com>
2018-08-28 13:17:29 +0800

    add legend, disabled 住宿 tabpage

--- test.js
+++ test.js
@@ -186,6 +186,7 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
+
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
